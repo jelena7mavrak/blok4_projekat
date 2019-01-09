f7f7065a4186f22b452346e4f526132297cbaf98
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2019-01-09 13:12:09 +0100

    fajl igrica

---
+++ MyPr/igrica.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QLabel
+from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtGui import QPainter, QColor, QBrush, QImage, QPalette, QIcon, QPixmap, QTransform
+import sys, random
+from  game import  Game
+from  board import Board
