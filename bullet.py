a4ca22f1d9026ac704a8753334f14f1dfb1b34aa
Kima7 <marinadimitrijevic07@gmail.com>
2019-01-09 18:54:37 +0100

    klasa bullet- update

--- bullet.py
+++ bullet.py
@@ -6,9 +6,16 @@ class Bullet(QLabel):
     def __init__(self,parent,x,y,pic):
         super(Bullet,self).__init__(parent)
 
-        if(pic == '2674.png'):
+        if(pic == 'bullet-heart.png'):
             self.dimX = 20
             self.dimY = 20
         else:
             self.dimX = 10
             self.dimY = 15
+        #izgled metka, velicina i pozicija na pozadini
+        bullet = QPixmap(pic)
+        bullet = bullet.scaled(self.dimX,self.dimY)
+        self.setPixmap(bullet)
+        self.x = x
+        self.y = y
+        self.setGeometry(x,y,self.dimX,self.dimY)
